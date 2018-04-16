f2305c634940b2fb4a58a7e74f8e6c24fab205d4
GuilhermeEd <guilherme.eduardo92@hotmail.com>
2018-04-15 22:26:52 -0300

    Added EnemySpawner

---
+++ Assets/EnemySpawners/EnemySpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+  [SerializeField] GameObject enemy;
+  [SerializeField] float spawnInterval = 3f;
+
+  Vector3 velocity;
+
+  void Start()
+  {
+    StartCoroutine(SpawnCorroutine());
+  }
+
+  IEnumerator SpawnCorroutine()
+  {
+    while (true)
+    {
+      GameObject enemyInstance = Instantiate(enemy, transform.position, Quaternion.identity);
+      enemyInstance.transform.parent = transform;
+    	yield return new WaitForSeconds(spawnInterval);
+    }
+  }
+
+}
